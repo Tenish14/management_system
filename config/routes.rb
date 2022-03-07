31e140e41438866c37220f3c60eba527310314a7
Tenish14 <Tenish02@gmail.com>
2022-03-07 13:36:51 +0800

    Finish user sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/cripsy_burger', as: 'rails_admin'
-  get 'user/new'
   root 'static_pages#home'
+  get '/signup',  to: 'users#new'
+
+  resources :users
 end
