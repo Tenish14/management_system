bf8a308a3ab40a0174c8a92b7e917a3126f3804f
Tenish14 <Tenish02@gmail.com>
2022-03-11 13:42:58 +0800

    Minor changes

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,10 +1,3 @@
 class Role < ApplicationRecord
-<<<<<<< HEAD
-    belongs_to :user, class_name: "User", optional: true
-
-    validates :name, presence: true
-    validates :description, presence: true
-=======
     has_many :users
->>>>>>> add-associate-tables
 end
