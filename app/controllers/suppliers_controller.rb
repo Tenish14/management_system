acc17f4e2fa3bd801b20ec8022eb01ab5a4c5539
Tenish14 <Tenish02@gmail.com>
2022-04-18 14:41:28 +0800

    Fix supplier form

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -47,8 +47,26 @@ class SuppliersController < ApplicationController
 
   def update
     if @supplier.update(supplier_params)
-      flash[:warning] = "Supplier Update"
-      redirect_to supplier_path(@supplier)
+      item_code = params[:supplier_item][:item_ids]
+      item_id  = Item.where(:item_code => item_code).pluck(:id)
+      item_code.replace(item_id)
+
+      location_code = params[:supplier_item][:location_ids]
+      location_id = Location.where(:location_code => location_code).pluck(:id)
+      location_code.replace(location_id)
+
+      item_params = params.require(:supplier_item).permit(item_ids: [], location_ids:[], supplier_prices: [])
+      items = item_params[:item_ids].zip(*item_params.values_at(:location_ids, :supplier_prices))
+      items.map do |item_id, location_id, supplier_price|
+        supplier_item = SupplierItem.update({
+          item_id: item_id,
+          location_id: location_id,
+          supplier_price: supplier_price,
+          supplier_id: @supplier.id
+        })
+        end
+        flash[:warning] = "Supplier updated"
+        redirect_to supplier_path(@supplier)
     else
       render 'edit'
     end
